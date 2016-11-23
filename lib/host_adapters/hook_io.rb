21e6b18f21cd6a6c524c3fa4c55edbd1c7be6630
Mike Nicholaides <mike.nicholaides@gmail.com>
2016-11-23 09:43:57 -0500

    Improve Hook.io adapter

--- lib/host_adapters/hook_io.rb
+++ lib/host_adapters/hook_io.rb
@@ -1,6 +1,6 @@
 require_relative '../boulevard/crypt'
 
-code_package = Hook['params']['__code_package__']
+code_package = Hook['params'].fetch('__code_package__')
 
 code = Boulevard::Crypt.new($secret_key).unpackage(code_package)
 
