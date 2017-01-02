969fdef7c04e56e5ed514d3adbe1f7a0a729a29f
Mike Nicholaides <mike.nicholaides@gmail.com>
2017-01-02 12:31:11 -0500

    Improve CLI tests

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,27 +2,20 @@ require "spec_helper"
 
 describe 'CLI', type: :aruba do
   it 'can round-trip it' do
-    run 'boulevard generate-key'
-    expect(last_command_started).to be_successfully_executed
-    key = last_command_started.output.strip
-
-    run "boulevard generate-host-code --secret-key '#{key}' --host-type local_script"
-    expect(last_command_started).to be_successfully_executed
-    local_script = last_command_started.output
+    key = sh 'boulevard generate-key'
 
     write_file 'script.rb', 'print "Hello World"'
-    run "boulevard package-code --secret-key '#{key}' script.rb"
-    expect(last_command_started).to be_successfully_executed
-    package = last_command_started.output.strip
 
-    write_file 'package', package
-    run "boulevard unpackage-code --secret-key '#{key}' package"
-    expect(last_command_started).to be_successfully_executed
+    write_file 'local_script_runner.rb',
+      sh("boulevard generate-host-code --secret-key '#{key}' --host-type local_script")
 
+    package = sh("boulevard package-code --secret-key '#{key}' script.rb")
+    write_file 'package.blvd', package
 
-    write_file 'local_script_runner.rb', local_script
-    run "ruby local_script_runner.rb '#{package}'"
-    expect(last_command_started).to have_output 'Hello World'
-    expect(last_command_started).to be_successfully_executed
+    expect {
+      sh "boulevard unpackage-code --secret-key '#{key}' package.blvd"
+    }.to_not raise_exception
+
+    expect(sh "ruby local_script_runner.rb '#{package}'").to eq 'Hello World'
   end
 end
--- spec/support/aruba.rb
+++ spec/support/aruba.rb
@@ -5,3 +5,15 @@ ENV['PATH'] = [
 ].join(File::PATH_SEPARATOR)
 
 require 'aruba/rspec'
+
+module RSpecArubaHelpers
+  def sh(str)
+    run str
+    expect(last_command_started).to be_successfully_executed
+    last_command_started.output.strip
+  end
+end
+
+RSpec.configure do |config|
+  config.include RSpecArubaHelpers, type: :aruba
+end
